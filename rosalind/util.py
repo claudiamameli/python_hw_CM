3875baad4073a9cd8432405dc2960334f800adc0
claudiamameli <claudiamameli.work@gmail.com>
2025-11-20 20:17:10 +0100

    Added newly created functions for fasta formats and GC contents

--- rosalind/util.py
+++ rosalind/util.py
@@ -21,3 +21,55 @@ def read_file_into_lines(file_path):
             cleaned = line.rstrip()
             lines.append(cleaned)
     return lines
+
+
+
+def text_to_fasta(fasta_file):
+    """
+    This function takes an already imported fasta file separated
+    in lines (use read_file_into_lines()) and returns a dictionary 
+    with unique identifiers as keys and DNA strings as values.
+    """
+    fasta_dic = {}
+    for line in fasta_file:
+        if line.startswith('>'): # this is a better convention because sometimes we might have '>' in other lines as well
+            key = line[1:]
+            fasta_dic[key] = ""
+        else:
+            fasta_dic[key] += line # more concise way
+    return fasta_dic
+
+
+
+def gc_content_count(fasta_dic):
+    """
+    This function calculates the GC contents of various DNA strings. 
+    Takes a dictionary formatted as {"ID" : "DnaString"} and returns
+    another dictionary with percentages for each "ID".
+    """
+    percentages = {}
+    for key in fasta_dic.keys():
+        value = fasta_dic[key]
+        percent = ((value.count('C') + value.count('G'))/len(value))
+        percentages[key] = percent * 100
+    return percentages
+
+
+# This dictionary might be useful in the future. 
+codon_dict = {
+'UUU': "F",      'CUU': 'L',      'AUU': 'I',      'GUU': 'V',
+'UUC': "F",      'CUC': 'L',      'AUC': 'I',      'GUC': 'V',
+'UUA': "L",      'CUA': 'L',      'AUA': 'I',      'GUA': 'V',
+'UUG': "L",      'CUG': 'L',      'AUG': 'M',      'GUG': 'V',
+'UCU': "S",      'CCU': 'P',      'ACU': 'T',      'GCU': 'A',
+'UCC': "S",      'CCC': 'P',      'ACC': 'T',      'GCC': 'A',
+'UCA': "S",      'CCA': 'P',      'ACA': 'T',      'GCA': 'A',
+'UCG': "S",      'CCG': 'P',      'ACG': 'T',      'GCG': 'A',
+'UAU': "Y",      'CAU': 'H',      'AAU': 'N',      'GAU': 'D',
+'UAC': "Y",      'CAC': 'H',      'AAC': 'N',      'GAC': 'D',
+'UAA': "",   'CAA': 'Q',      'AAA': 'K',      'GAA': 'E',
+'UAG': "",   'CAG': 'Q',      'AAG': 'K',      'GAG': 'E',
+'UGU': "C",      'CGU': 'R',      'AGU': 'S',      'GGU': 'G',
+'UGC': "C",      'CGC': 'R',      'AGC': 'S',      'GGC': 'G',
+'UGA': "",   'CGA': 'R',      'AGA': 'R',      'GGA': 'G',
+'UGG': "W",      'CGG': 'R',      'AGG': 'R',      'GGG': 'G'}
